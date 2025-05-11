3303c2eaa80c2d9441eb57051e82999aca4d1328
Louis F. <me@itslouis.dev>
2025-05-12 00:06:33 +0200

    chore(dep): add more linting related packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,23 @@
 module.exports = {
   env: {
+    browser: true,
     node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+  ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@babel/eslint-parser',
     ecmaVersion: 'latest',
     sourceType: 'module',
+    requireConfigFile: false,
   },
+  plugins: ['vue', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'vue/multi-word-component-names': 'off',
   },
-  plugins: ['prettier'],
-};
+}
