4dc6d2abea7639ef2ab9c65dd89910df6d17b3e2
Louis F. <me@itslouis.dev>
2025-05-12 00:05:03 +0200

    feat(thread): setup the frontend to display messages

---
+++ apps/thread/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Messages du forum</h1>
+    <ul>
+      <li v-for="thread in threads" :key="thread.id">
+        <strong>{{ thread.title }}</strong
+        ><br />
+        {{ thread.content }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const threads = ref([])
+onMounted(async () => {
+  const res = await fetch('http://api:3000/threads')
+  threads.value = await res.json()
+})
+</script>
